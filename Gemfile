b426dc38282701d342dfdea4f7cb0fb989a1a112
Candidate 9 <frankie+candidate9@frankieroberto.com>
2012-06-24 12:37:57 +0100

    Initial Rails app

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+# Use SQLite3 for easier develoment and testing
+group :development, :test do
+  gem 'sqlite3'
+end
+
+# Use PostGres in production
+group :production do
+  gem 'pg'
+end
+
+gem 'json'
+gem 'thin'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localpoll::Application.config.secret_token = '3b6c3b622bcca379c0b2f816eebb71589be883930309861f11609e859e99ee4612dbabda29476d7199f3b42459a06a40d87d7bf5b2dbb1613c86fc5f336647d2'
